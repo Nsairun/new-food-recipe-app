709d6be0e9bbe06fd1bf5409f5f78d5e02b53ce6
unknown <emmansairun@gmail.com>
2023-06-29 14:18:12 +0100

    added more features

--- src/components/page/LandingPage1.jsx
+++ src/components/page/LandingPage1.jsx
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { useNavigate } from 'react';
 import './landingPage.css';
 
 function LandingPage1() {
+  const navigate = useNavigate();
+
+  function handleClick() {
+    navigate('/');
+  }
   return (
     <div className="landing">
       <h1>WELCOME TO OUR RESTAURANT</h1>
       <p>we have a variety of african meals to offer you</p>
+      <button type="button" onSubmit={handleClick}>
+        Next Page
+      </button>
     </div>
   );
 }
